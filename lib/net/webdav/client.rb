c000dff68edf9f5cc7051ecaf1e0833fa0917aa9
Joshua McClintock <joshua@navigatingcancer.com>
2014-03-04 13:13:45 -0800

    fixed for our envrionment

--- lib/net/webdav/client.rb
+++ lib/net/webdav/client.rb
@@ -12,7 +12,7 @@ module Net
         @host = "#{scheme}://#{hostname}#{port.nil? ? "" : ":" + port}"
         @http_auth_types = options[:http_auth_types] || :basic
 
-        unless userinfo.nil?
+        unless userinfo.nil?            
           @username, @password = userinfo.split(':')
         else
           @username = options[:username]
@@ -21,58 +21,21 @@ module Net
 
         @url = URI.join(@host, path)
       end
-
+      
       def file_exists? path
         response = Curl::Easy.http_head full_url(path)
         response.response_code >= 200 && response.response_code <= 209
       end
-
+      
       def get_file remote_file_path, local_file_path
-        begin
-
-          file = output_file(local_file_path)
-
-          connection = Curl::Easy.new
-          connection.userpwd = curl_credentials if @username.present? && @password.present?
-          connection.url = full_url(remote_file_path)
-          connection.perform
-
-          notify_of_error(connection, "getting file. #{remote_file_path}")  unless connection.response_code == 200
-
-          file.write(connection.body_str)
-        ensure
-          file.close
-        end
+        Curl::Easy.download full_url(remote_file_path), local_file_path
       end
 
-      def put_file path, file, create_path = false
-        connection = Curl::Easy.http_head full_url(path), &method(:auth)
-
-        if create_path
-          scheme, userinfo, hostname, port, registry, path, opaque, query, fragment = URI.split(full_url(path))
-          path_parts = path.split('/').reject {|s| s.nil? || s.empty?}
-          path_parts.pop
-
-          for i in 0..(path_parts.length - 1)
-            #if the part part is for a file with an extension skip
-            next if File.extname(path_parts[i]).present?
-
-            parent_path = path_parts[0..i].join('/')
-            url = URI.join("#{scheme}://#{hostname}#{(port.nil? || port == 80) ? "" : ":" + port}/", parent_path)
-            connection.url = full_url( url )
-            connection.http(:MKCOL)
-            notify_of_error(connection, "creating directories") unless (connection.response_code == 201 || connection.response_code == 204 || connection.response_code == 405)
-            return connection.response_code unless connection.response_code == 201 || connection.response_code == 405 # 201 Created or 405 Conflict (already exists)
-          end
-        end
-        connection.url = full_url(path)
-        connection.http_put file
-        notify_of_error(connection, "creating(putting) file. File path: #{path}") unless (connection.response_code == 201 || connection.response_code == 204)
-        connection.response_code
-      end
-
-      def notify_of_error(connection, action)
-        raise "Error in WEBDav Client while #{action} with error: #{connection.status}"
+      def put_file path, file, create_path = true
+        make_directory(path)
+        curl = Curl::Easy.http_put full_url(path), file, &method(:auth)
+        raise curl.status unless [201, 204, 301].include?(curl.response_code)
+        curl 
       end
 
       def delete_file path
@@ -80,33 +43,29 @@ module Net
       end
 
       def make_directory path
-        curl = Curl::Easy.new(full_url(path))
-        auth(curl)
-        curl.http(:MKCOL)
-        curl
+        scheme, userinfo, hostname, port, registry, path, opaque, query, fragment = URI.split(full_url(path))
+        path_parts = path.split('/').reject {|s| s.nil? || s.empty?}
+        extpath = Array.new
+        path_parts.pop # take file name off the end
+        path_parts.each do |part|
+          parent_path = extpath.push(part).join("/")
+          url = URI.join("#{scheme}://#{hostname}#{(port.nil? || port == 80) ? "" : ":" + port}/", parent_path)
+          curl = Curl::Easy.new(full_url(url))
+          auth(curl)
+          curl.http(:MKCOL)
+          curl
+        end
       end
-
+      
       private
-      def curl_credentials
-        "#{@username}:#{@password}"
-      end
-
       def auth curl
-        curl.userpwd = curl_credentials
+        curl.username = @username unless @username.nil?
+        curl.password = @password unless @password.nil?
         curl.http_auth_types = @http_auth_types unless @http_auth_types.nil?
       end
-
+      
       def full_url path
         URI.join(@url, path).to_s
-      end
-
-      def output_file(filename)
-        if filename.is_a? IO
-          filename.binmode if filename.respond_to?(:binmode)
-          filename
-        else
-          File.open(filename, 'wb')
-        end
       end
     end
   end
